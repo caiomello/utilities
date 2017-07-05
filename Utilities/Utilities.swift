bb44cf031052cc4c34e5b86da06194932239589f
Caio Mello <github@caiomello.com>
2017-07-05 19:32:23 -0300

    Set date formatter time zone

--- Utilities/Utilities.swift
+++ Utilities/Utilities.swift
@@ -14,6 +14,7 @@ extension DateFormatter {
 	public static let shared: DateFormatter = {
 		let formatter = DateFormatter()
 		formatter.locale = Locale(identifier: "en_US_POSIX")
+		formatter.timeZone = TimeZone(secondsFromGMT: 0)
 		formatter.isLenient = true
 		return formatter
 	}()
