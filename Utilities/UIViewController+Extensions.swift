dd16a3a3fbabeb0d061958d6ff8cc75063b4050f
Caio Mello <github@caiomello.com>
2019-08-21 15:03:55 -0300

    Add view controller extensions

---
+++ Utilities/UIViewController+Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  UIViewController+Extensions.swift
+//  Utilities
+//
+//  Created by Caio Mello on 8/21/19.
+//  Copyright © 2019 Caio Mello. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    public class func instantiate<T: UIViewController>(_ viewController: @escaping (NSCoder) -> T?) -> T {
+        let storyboard = UIStoryboard(name: "\(T.self)", bundle: nil)
+
+        return storyboard.instantiateInitialViewController(creator: viewController)!
+    }
+
+    public func show<T: UIViewController>(_ viewController: @escaping (NSCoder) -> T?) {
+        let viewController = T.instantiate(viewController)
+        show(viewController, sender: nil)
+    }
+
+    public func showDetail<T: UIViewController>(_ viewController: @escaping (NSCoder) -> T?) {
+        let viewController = T.instantiate(viewController)
+
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.barStyle = .black
+
+        showDetailViewController(navigationController, sender: nil)
+    }
+
+    public func present<T: UIViewController>(_ viewController: @escaping (NSCoder) -> T?) {
+        let viewController = T.instantiate(viewController)
+
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.barStyle = .black
+
+        present(navigationController, animated: true)
+    }
+}
