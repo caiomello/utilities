15bb0e5b2e2c0e1ed36763d5877c566d4f1c0a44
Caio Mello <github@caiomello.com>
2020-05-09 16:49:17 +0200

    Add TextInputViewController

---
+++ Sources/Utilities/Protocols & Classes/TextInputViewController.swift
@@ -0,0 +1,58 @@
+//
+//  TextInputViewController.swift
+//  Utilities
+//
+//  Created by Caio Mello on 09.05.20.
+//  Copyright © 2020 Caio Mello. All rights reserved.
+//
+
+import UIKit
+
+open class TextInputViewController: UIViewController {
+    @IBOutlet public var scrollView: UIScrollView!
+}
+
+// MARK: - Lifecycle
+
+extension TextInputViewController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillChangeFrameNotification,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+}
+
+// MARK: - Notifications
+
+extension TextInputViewController {
+    @objc private func adjustForKeyboard(notification: Notification) {
+        let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)!.cgRectValue
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            scrollView.contentInset.bottom = 0
+        } else {
+            let inset = view.convert(keyboardRect, from: nil).size.height
+            scrollView.contentInset.bottom = inset
+            scrollView.verticalScrollIndicatorInsets.bottom = inset
+        }
+    }
+}
